ccea15d68ee91ee2c05d773ef8a46757103e944f
Jeff Cole <coleti16@students.ecu.edu>
2018-02-14 12:17:33 -0500

    things

--- csv_concat.py
+++ csv_concat.py
@@ -18,6 +18,7 @@ else:
     print("\nplease specify input file name using -neg <file_name> \n")
     sys.exit()
 
+df_dict = {}
 
 def path_file(file,opt):
     with open(file) as f:
@@ -25,16 +26,16 @@ def path_file(file,opt):
 
             currentFile = line.strip()
             print(currentFile)
+            print(currentFile.split("/")[-2])
             currentData = pd.read_csv(currentFile, header=None)
-            # print(list(currentData))
             currentHeaders = list(currentData)
             newHeaders = ["N:feature_" + str(header) for header in currentHeaders]
             currentData.columns = newHeaders
-            print(currentHeaders)
-            print(newHeaders)
+            # print(currentHeaders)
+            # print(newHeaders)
             currentData['C:venom'] = opt
-            print(list(currentData))
-            print(currentData.shape)
+            # print(list(currentData))
+            # print(currentData.shape)
 
 path_file(pos_path,1)
 path_file(neg_path,0)
