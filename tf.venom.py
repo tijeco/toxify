c4e4892cc5d6a1caf9ca141ae484f836e79a4480
Jeff Cole <coleti16@students.ecu.edu>
2018-03-13 10:35:29 -0400

    old version

--- tf.venom.py
+++ tf.venom.py
@@ -52,7 +52,7 @@ test_set = tf.contrib.learn.datasets.base.load_csv_with_header(
     filename=test_data,
     target_dtype=np.int,
     features_dtype=np.float32)
-feature_columns = [tf.feature_column.numeric_column("x", shape=[data_shape])]
+feature_columns = [tf.contrib.layers.real_valued_column("", dimension=data_shape)]
 print(feature_columns)
 classifier = tf.estimator.DNNClassifier(feature_columns=feature_columns,
                                   hidden_units=[500,500,500],
