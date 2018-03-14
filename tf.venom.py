44d455ff3da020de752b8adc44b89b3e2ff1986b
Jeff Cole <coleti16@students.ecu.edu>
2018-03-14 11:01:04 -0400

    updated logic

--- tf.venom.py
+++ tf.venom.py
@@ -23,17 +23,17 @@ def getOptionValue(option):
 
 train_only = False
 test_only = False
-train_and_test = False
+# train_and_test = False
 if "-train" in sys.argv or "-test" in sys.argv:
 
     if "-train" in sys.argv:
         training_data = getOptionValue("-train")
         train_only = True
-    if "-test" in sys.argv:
+    else:
         test_data = getOptionValue("-test")
         test_only = True
-    if train_only and test_only:
-        train_and_test = True
+    # if train_only and test_only:
+    #     train_and_test = True
 
 else:
     print("please provide training data with -train")
@@ -41,51 +41,54 @@ else:
     sys.exit()
 
 
+if train_only:
+    training_set = tf.contrib.learn.datasets.base.load_csv_with_header(
+        filename=training_data,
+        target_dtype=np.int,
+        features_dtype=np.float32)
+    with open(training_data) as f:
+        for line in f:
+            row = line.strip().split(",")
+            if len(row) ==2:
+                data_shape = int(row[1])
+            else:
+                break
+    print("NUM FEATURES:",data_shape)
+    feature_columns = [tf.feature_column.numeric_column("x", shape=[data_shape])]
+    print(feature_columns)
+    classifier = tf.estimator.DNNClassifier(feature_columns=feature_columns,
+                                      hidden_units=[500,500,500],
+                                          n_classes=2,
+                                          # dropout=0.02,
+                                          model_dir="tmp/venom_model",
+                                          optimizer=tf.train.ProximalAdagradOptimizer(learning_rate=0.01, l1_regularization_strength=0.001)
+                                          )
+                                          #Adagrad', 'Adam', 'Ftrl', 'RMSProp', 'SGD'
+    train_input_fn = tf.estimator.inputs.numpy_input_fn(
+        x={"x": np.array(training_set.data)},
+        y=np.array(training_set.target),
+        num_epochs=3,
+        shuffle=True)
+    classifier.train(input_fn=train_input_fn, steps=1000)
 
-training_set = tf.contrib.learn.datasets.base.load_csv_with_header(
-    filename=training_data,
-    target_dtype=np.int,
-    features_dtype=np.float32)
-with open(training_data) as f:
-    for line in f:
-        row = line.strip().split(",")
-        if len(row) ==2:
-            data_shape = int(row[1])
-        else:
-            break
-print("NUM FEATURES:",data_shape)
 # data_shape = training_set.shape[1]
-test_set = tf.contrib.learn.datasets.base.load_csv_with_header(
-    filename=test_data,
-    # na_value='NaN'
-    target_dtype=np.int,
-    features_dtype=np.float32)
-feature_columns = [tf.feature_column.numeric_column("x", shape=[data_shape])]
-print(feature_columns)
-classifier = tf.estimator.DNNClassifier(feature_columns=feature_columns,
-                                  hidden_units=[500,500,500],
-                                      n_classes=2,
-                                      # dropout=0.02,
-                                      model_dir="tmp/venom_model",
-                                      optimizer=tf.train.ProximalAdagradOptimizer(learning_rate=0.01, l1_regularization_strength=0.001)
-                                      )
-                                      #Adagrad', 'Adam', 'Ftrl', 'RMSProp', 'SGD'
+if test_only:
+    test_set = tf.contrib.learn.datasets.base.load_csv_with_header(
+        filename=test_data,
+        # na_value='NaN'
+        target_dtype=np.int,
+        features_dtype=np.float32)
 
-train_input_fn = tf.estimator.inputs.numpy_input_fn(
-    x={"x": np.array(training_set.data)},
-    y=np.array(training_set.target),
-    num_epochs=3,
-    shuffle=True)
-classifier.train(input_fn=train_input_fn, steps=1000)
 
-test_input_fn = tf.estimator.inputs.numpy_input_fn(
-    x={"x": np.array(test_set.data)},
-    y=np.array(test_set.target),
-    num_epochs=1,
-    shuffle=False)
-accuracy_score = classifier.evaluate(input_fn=test_input_fn)["accuracy"]
 
-print("\nTest Accuracy: {0:f}\n".format(accuracy_score))
+    test_input_fn = tf.estimator.inputs.numpy_input_fn(
+        x={"x": np.array(test_set.data)},
+        y=np.array(test_set.target),
+        num_epochs=1,
+        shuffle=False)
+    accuracy_score = classifier.evaluate(input_fn=test_input_fn)["accuracy"]
+
+    print("\nTest Accuracy: {0:f}\n".format(accuracy_score))
 
 
 """
