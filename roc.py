be5df96a36c28310ae3e74cdab7973652d5a0ac1
Jeff Cole <coleti16@students.ecu.edu>
2018-03-14 19:17:43 -0400

    now?

--- roc.py
+++ roc.py
@@ -6,10 +6,10 @@ df = pd.read_csv(sys.argv[1],header=None)
 # print(df[2]
 # print(df.loc[df[2] == 0])
 pos_df = df.loc[df[2] == 0]
-print(df.shape)
+# print(df.shape)
 for i in range(501,1000):
-    TP=pos_df.loc[pos_df[0] > i/1000.0].shape[0]
-    FP=pos_df.loc[pos_df[1] > i/1000.0].shape[0]
+    TP=pos_df.loc[pos_df[0] > i/1000.0].shape[0]/df.shape
+    FP=pos_df.loc[pos_df[1] > i/1000.0].shape[0]/df.shape
     print(i,TP,FP)
 # print(pos_df.loc[pos_df[0] > 0.9])
 # print(pos_df.loc[pos_df[0] > 0.9].shape)
