b5a1ce7e16cf654fe2474b606fac5336d0ba844e
Jeff Cole <coleti16@students.ecu.edu>
2018-03-14 18:53:34 -0400

    shape

--- roc.py
+++ roc.py
@@ -7,4 +7,6 @@ print(list(df))
 print(df.loc[df[2] == 0])
 pos_df = df.loc[df[2] == 0]
 print(pos_df.loc[pos_df[0] > 0.9])
-print(pos_df.loc[pos_df[1] > 0.9])
+print(pos_df.loc[pos_df[0] > 0.9].shape)
+print(pos_df.loc[pos_df[0] > 0.9])
+print(pos_df.loc[pos_df[0] > 0.9].shape)
