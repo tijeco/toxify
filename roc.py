e34c90278ee29b87895e8a328699acd2ca9b6d20
Jeff Cole <coleti16@students.ecu.edu>
2018-03-14 19:37:31 -0400

    flipp

--- roc.py
+++ roc.py
@@ -15,8 +15,8 @@ for i in range(1,1000):
 neg_df = df.loc[df[2] == 1]
 # print(df.shape)
 for i in range(1,1000):
-    TP=neg_df.loc[neg_df[0] > i/1000.0].shape[0]/neg_df.shape[0]
-    FP=neg_df.loc[neg_df[1] > i/1000.0].shape[0]/neg_df.shape[0]
+    FP=neg_df.loc[neg_df[0] > i/1000.0].shape[0]/neg_df.shape[0]
+    TP=neg_df.loc[neg_df[1] > i/1000.0].shape[0]/neg_df.shape[0]
     print(i,TP,FP)
 # print(pos_df.loc[pos_df[0] > 0.9])
 # print(pos_df.loc[pos_df[0] > 0.9].shape)
