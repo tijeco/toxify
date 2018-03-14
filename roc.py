e1d47ecd9b8fbae8e84f6800db0533adcaba5ff0
Jeff Cole <coleti16@students.ecu.edu>
2018-03-14 19:15:27 -0400

    comment

--- roc.py
+++ roc.py
@@ -2,9 +2,9 @@ import pandas as pd
 import sys
 
 df = pd.read_csv(sys.argv[1],header=None)
-print(list(df))
+# print(list(df))
 # print(df[2]
-print(df.loc[df[2] == 0])
+# print(df.loc[df[2] == 0])
 pos_df = df.loc[df[2] == 0]
 for i in range(501,1000):
     TP=pos_df.loc[pos_df[0] > i/1000.0].shape[0]
