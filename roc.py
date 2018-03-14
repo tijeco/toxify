bf3d86c82af4c3939a8c7524fc533a7f75adc713
Jeff Cole <coleti16@students.ecu.edu>
2018-03-14 19:14:48 -0400

    TP

--- roc.py
+++ roc.py
@@ -6,7 +6,11 @@ print(list(df))
 # print(df[2]
 print(df.loc[df[2] == 0])
 pos_df = df.loc[df[2] == 0]
-print(pos_df.loc[pos_df[0] > 0.9])
-print(pos_df.loc[pos_df[0] > 0.9].shape)
-print(pos_df.loc[pos_df[1] > 0.9])
-print(pos_df.loc[pos_df[1] > 0.9].shape)
+for i in range(501,1000):
+    TP=pos_df.loc[pos_df[0] > i/1000.0].shape[0]
+    FP=pos_df.loc[pos_df[1] > 0.9].shape[0]
+    print(i,TP,FP)
+# print(pos_df.loc[pos_df[0] > 0.9])
+# print(pos_df.loc[pos_df[0] > 0.9].shape)
+# print(pos_df.loc[pos_df[1] > 0.9])
+# print(pos_df.loc[pos_df[1] > 0.9].shape)
