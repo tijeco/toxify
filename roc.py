24733f7c6e48aa925d45a8356f361b4cbaf8681b
Jeff Cole <coleti16@students.ecu.edu>
2018-03-16 09:33:58 -0400

    new

--- roc.py
+++ roc.py
@@ -18,6 +18,7 @@ for i in range(1,1000):
     FP=neg_df.loc[neg_df[0] > i/1000.0].shape[0]/neg_df.shape[0]
     TP=neg_df.loc[neg_df[1] > i/1000.0].shape[0]/neg_df.shape[0]
     print(i,TP,FP)
+print(neg_df)
 # print(pos_df.loc[pos_df[0] > 0.9])
 # print(pos_df.loc[pos_df[0] > 0.9].shape)
 # print(pos_df.loc[pos_df[1] > 0.9])
