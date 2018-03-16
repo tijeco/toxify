7eafe8db872e804c965497e7dd6c81ff908bdb86
Jeff Cole <coleti16@students.ecu.edu>
2018-03-16 09:41:26 -0400

    print less

--- roc.py
+++ roc.py
@@ -17,7 +17,7 @@ neg_df = df.loc[df[2] == 1]
 for i in range(1,1000):
     FP=neg_df.loc[neg_df[0] > i/1000.0].shape[0]/neg_df.shape[0]
     TP=neg_df.loc[neg_df[1] > i/1000.0].shape[0]/neg_df.shape[0]
-    print(i,TP,FP)
+    # print(i,TP,FP)
 print(neg_df.describe())
 print(pos_df.describe())
 # print(pos_df.loc[pos_df[0] > 0.9])
