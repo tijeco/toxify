cdc1c9f55ce9ec6cbc19bdc9e2e508062d5e58a9
Jeff Cole <coleti16@students.ecu.edu>
2018-03-31 19:54:08 -0400

    setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='toxify',
+      version='0.1',
+      description='The toxify joke in the world',
+      url='http://github.com/tijeco/toxify',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['toxify'],
+      zip_safe=False)
