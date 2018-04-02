0c1f9a39637905e9954989312d5a76e778b8ee16
Jeff Cole <coleti16@students.ecu.edu>
2018-04-02 14:47:34 -0400

    hack

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 
 
 from setuptools import setup
-
+CURRENT_ABSOLUTE = os.path.dirname(os.path.realpath(__file__))
+# replace LIB_PATH definition
+LIB_PATH = [libpath['find_lib_path']()[0].replace(CURRENT_ABSOLUTE + '/', '')]
 setup(name='toxify',
       version='0.1',
       description='The toxify joke in the world',
