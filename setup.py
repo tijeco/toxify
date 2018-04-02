b9189a1adb409d82065c49d9471cdfe101451bba
Jeff Cole <coleti16@students.ecu.edu>
2018-04-02 14:53:42 -0400

    cleaned

--- setup.py
+++ setup.py
@@ -1,9 +1,6 @@
 
-import os
 from setuptools import setup
-CURRENT_ABSOLUTE = os.path.dirname(os.path.realpath(__file__))
 # replace LIB_PATH definition
-LIB_PATH = [libpath['find_lib_path']()[0].replace(CURRENT_ABSOLUTE + '/', '')]
 setup(name='toxify',
       version='0.1',
       description='The toxify joke in the world',
