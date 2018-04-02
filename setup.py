0b61adca7e094dd4e273af8f0a293d132f0d660e
Jeff Cole <coleti16@students.ecu.edu>
2018-04-02 16:05:36 -0400

    nope

--- setup.py
+++ setup.py
@@ -11,5 +11,4 @@ setup(name='toxify',
       license='GNU',
       packages=['toxify'],
       package_data={'toxify':['data/RaptorX_Property_Fast/']},
-      include_package_data = True,
       zip_safe=False)
