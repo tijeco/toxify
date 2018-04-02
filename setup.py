64ab802d765bd14429d74c908ef8c9b82fdd18ea
Jeff Cole <coleti16@students.ecu.edu>
2018-04-02 16:28:46 -0400

    now

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 
-from setuptools import setup, find_packages
+from distutils.core import setup
+#from setuptools import setup, find_packages
 # replace LIB_PATH definition
 setup(name='toxify',
       version='0.1',
@@ -10,5 +11,6 @@ setup(name='toxify',
       author_email='coleti16@students.ecu.edu',
       license='GNU',
       packages=['toxify'],
+      package_data={'toxify':['data/RaptorX_Property_Fast/']},
       include_package_data = True,
       zip_safe=False)
