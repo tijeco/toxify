5247f8d5343eb98ffa65bf9ba2f4f5ec0aa8695a
Jeff Cole <coleti16@students.ecu.edu>
2018-04-02 16:33:23 -0400

    new

--- setup.py
+++ setup.py
@@ -11,6 +11,5 @@ setup(name='toxify',
       author_email='coleti16@students.ecu.edu',
       license='GNU',
       packages=['toxify'],
-      package_data={'toxify':['data/RaptorX_Property_Fast/']},
       include_package_data = True,
       zip_safe=False)
