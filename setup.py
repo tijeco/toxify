c7cda5b3dc8736d2d01135306716971c67a3082e
Jeff Cole <coleti16@students.ecu.edu>
2018-07-17 18:14:24 -0400

    set up package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='toxify',
+      version='0.1',
+      description='The toxify joke in the world',
+      url='http://github.com/tijeco/toxify',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      entry_points = {'console_scripts': ['toxify=toxify.cli:main'],},
+      license='MIT',
+      packages=['toxify'],
+      include_package_data=True,
+      zip_safe=False)
