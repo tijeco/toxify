ca738d2d85441d503f4f55f98046a6685cb16df3
Jeff Cole <coleti16@students.ecu.edu>
2018-10-10 18:00:53 -0400

    0.1.34

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='toxify',
-      version='0.1.33',
+      version='0.1.34',
       description='The toxify joke in the world',
       url='http://github.com/tijeco/toxify',
       author='Flying Circus',
