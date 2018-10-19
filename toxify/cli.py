f22a444a71fe97407ea4b2466f0d647432ed1586
Jeff Cole <coleti16@students.ecu.edu>
2018-10-18 21:25:16 -0400

    print

--- toxify/cli.py
+++ toxify/cli.py
@@ -86,6 +86,7 @@ def main():
             fa_mat.append(sw.seq2atchley(seq,0,500))
 
         # this will produce np array of fifteenmer seqs
+        print("saving to ",predictions_dir+"/protein_vectors.npy")
         np.save(predictions_dir+"/protein_vectors.npy",np.array(fa_mat))
         use15mer = False
 
